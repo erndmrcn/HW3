bd470a144ff524346b9355eb57950b57c7aa42f9
erndmrcn <erndmrcn42@gmail.com>
2021-12-10 18:43:19 +0300

    Minimax is done
    Minimax algorithm done.
    BUGS:
     * tictactoe visited nodes missing
     * alpha_beta will be implemented

--- hw3.py
+++ hw3.py
@@ -18,13 +18,7 @@ class Node:
 global stateNodes
 # holds (state, Node)
 stateNodes = []
-
-# empty tictactoe board
-tictactoe = [
-                [0, 0, 0], 
-                [0, 0, 0], 
-                [0, 0, 0]
-             ]
+visited = []
 
 def deepCopy(arr):
     result = []
@@ -36,97 +30,140 @@ def deepCopy(arr):
     return result
 
 def terminalState(state):
+    visited.append(state)
+
     row1 = state[:3]
     if row1[0] == row1[1] and row1[1] == row1[2]:
         # terminate
-        return True
+        if row1[0] == 'X':
+            return 'X'
+        else:
+            return 'Y'
 
     row2 = state[3:6]
     if row2[0] == row2[1] and row2[1] == row2[2]:
         # terminate
-        return True
+        if row2[0] == 'X':
+            return 'X'
+        else:
+            return 'Y'
 
     row3 = state[6:9]
     if row3[0] == row3[1] and row3[1] == row3[2]:
         # terminate
-        return True
+        if row3[0] == 'X':
+            return 'X'
+        else:
+            return 'Y'
 
     diagonal1 = state[0::4]
     if diagonal1[0] == diagonal1[1] and diagonal1[1] == diagonal1[2]:
         # terminage
-        return True
+        if diagonal1[0] == 'X':
+            return 'X'
+        else:
+            return 'Y'
 
     diagonal2 = state[2:8:2]
     if diagonal2[0] == diagonal2[1] and diagonal2[1] == diagonal2[2]:
         # terminage
-        return True
-    
+        if diagonal2[0] == 'X':
+            return 'X'
+        else:
+            return 'Y'  
+
     column1 = state[0::3]
     if column1[0] == column1[1] and column1[1] == column1[2]:
         # terminate
-        return True
+        if column1[0] == 'X':
+            return 'X'
+        else:
+            return 'Y'
 
     column2 = state[1::3]
     if column2[0] == column2[1] and column2[1] == column2[2]:
         # terminate
-        return True
+        if column2[0] == 'X':
+            return 'X'
+        else:
+            return 'Y'
 
     column3 = state[2::3]
     if column3[0] == column3[1] and column3[1] == column3[2]:
         # terminate
-        return True
+        if column3[0] == 'X':
+            return 'X'
+        else:
+            return 'Y'
 
-    if len(state) == 9:
-        return True
+    if ' ' not in state:
+        return 'BF'
+    
+    # non terminal
+    return 'NT'
 
 
-def expand(currentState, symbol):
+def expand(currentNode, depth):
     # Order -> (0, 0), (1, 0), (2, 0)
     #          (0, 1), (1, 1), (2, 1)
     #          (0, 2), (1, 2), (2, 2)
+    if depth % 2 == 0:
+        symbol = 'X'
+    elif depth % 2 == 1:
+        symbol = 'O'
+
     childs = []
-    arr = stateToArray(currentState)
+    arr = stateToArray(currentNode.state)
     
     if arr[0][0] == ' ':
         tempArr = deepCopy(arr)
         tempArr[0][0] = symbol
-        childs.append(arrayToState(tempArr))
-    if arr[1][0] == ' ':
-        tempArr = deepCopy(arr)
-        tempArr[1][0] = symbol
-        childs.append(arrayToState(tempArr))
-    if arr[2][0] == ' ':
-        tempArr = deepCopy(arr)
-        tempArr[2][0] = symbol
-        childs.append(arrayToState(tempArr))
-
-
+        newNode = Node(currentNode, None, arrayToState(tempArr), (0,0), None)
+        childs.append(newNode)
     if arr[0][1] == ' ':
         tempArr = deepCopy(arr)
         tempArr[0][1] = symbol
-        childs.append(arrayToState(tempArr))
-    if arr[1][1] == ' ':
-        tempArr = deepCopy(arr)
-        tempArr[1][1] = symbol
-        childs.append(arrayToState(tempArr))
-    if arr[2][1] == ' ':
-        tempArr = deepCopy(arr)
-        tempArr[2][1] = symbol
-        childs.append(arrayToState(tempArr))
-
-
+        newNode = Node(currentNode, None, arrayToState(tempArr), (1,0), None)
+        childs.append(newNode)
     if arr[0][2] == ' ':
         tempArr = deepCopy(arr)
         tempArr[0][2] = symbol
-        childs.append(arrayToState(tempArr))
+        newNode = Node(currentNode, None, arrayToState(tempArr), (2,0), None)
+        childs.append(newNode)
+
+
+    if arr[1][0] == ' ':
+        tempArr = deepCopy(arr)
+        tempArr[1][0] = symbol
+        newNode = Node(currentNode, None, arrayToState(tempArr), (0,1), None)
+        childs.append(newNode)
+    if arr[1][1] == ' ':
+        tempArr = deepCopy(arr)
+        tempArr[1][1] = symbol
+        newNode = Node(currentNode, None, arrayToState(tempArr), (1,1), None)
+        childs.append(newNode)
     if arr[1][2] == ' ':
         tempArr = deepCopy(arr)
-        tempArr[2][2] = symbol
-        childs.append(arrayToState(tempArr))
+        tempArr[1][2] = symbol
+        newNode = Node(currentNode, None, arrayToState(tempArr), (2,1), None)
+        childs.append(newNode)
+
+
+    if arr[2][0] == ' ':
+        tempArr = deepCopy(arr)
+        tempArr[2][0] = symbol
+        newNode = Node(currentNode, None, arrayToState(tempArr), (0,2), None)
+        childs.append(newNode)
+    if arr[2][1] == ' ':
+        tempArr = deepCopy(arr)
+        tempArr[2][1] = symbol
+        newNode = Node(currentNode, None, arrayToState(tempArr), (1,2), None)
+        childs.append(newNode)
     if arr[2][2] == ' ':
         tempArr = deepCopy(arr)
         tempArr[2][2] = symbol
-        childs.append(arrayToState(tempArr))
+        newNode = Node(currentNode, None, arrayToState(tempArr), (2,2), None)
+        childs.append(newNode)
 
     return childs
 
@@ -207,25 +244,67 @@ def readFile(fileName):
         exit('Program exited with code -1')
     return
 
-def findNode(state):
-    global root
-
-    return
 
 def minimax_decision_ttt():
+    global visited
     v_max = -2000000
-    v_min = 2000000
     v = []
     depth = 0
     childs = expand(root, depth)
+    for child in childs:
+        root.childs.append(child)
+
+    for child in root.childs:
+        v.append(max(v_max, min_value_ttt(child, depth + 1)))
+
+    result = max(v)
+    ind = v.index(result)
+    return result, root.childs[ind].action, visited
     
-    return
 
-def max_value_ttt(currentState):
-    return
+def max_value_ttt(currentNode, depth):
+    result = terminalState(currentNode.state)
+    if result == 'X':
+        # X wins
+        return 5 - (0.01 * (depth - 1))
+    elif result == 'Y':
+        # Y wins
+        return -5
+    elif result == 'BF':
+        # board full - no winner
+        return 0
+    
+    v = -2000000
+    childs = expand(currentNode, depth)
+    for child in childs:
+        currentNode.childs.append(child)
 
-def min_value_ttt(currentState):
-    return
+    for child in currentNode.childs:
+        v = max(v, min_value_ttt(child, depth+1))
+
+    return v
+
+def min_value_ttt(currentNode, depth):
+    result = terminalState(currentNode.state)
+    if result == 'X':
+        # X wins
+        return 5 - (0.01 * (depth - 1))
+    elif result == 'Y':
+        # Y wins
+        return -5
+    elif result == 'BF':
+        # board full - no winner
+        return 0
+    
+    v = 2000000
+    childs = expand(currentNode, depth)
+    for child in childs:
+        currentNode.childs.append(child)
+
+    for child in currentNode.childs:
+        v = min(v, max_value_ttt(child, depth+1))
+
+    return v
 
 def minimax_decision(min_max):
     v_max = -2000000
@@ -233,48 +312,43 @@ def minimax_decision(min_max):
     v = []
     if min_max == "MAX":
         for child in root.childs:
-            print(child.state)
+            visited.append(child.state)
             v.append(max(v_max, min_value(child)))
 
         result = max(v)    
-        print("main: max_value " + str(v) + " is selected")
-
-        return result
+        ind = v.index(result)
+        return result, root.childs[ind].action, visited
     elif min_max == "MIN":
         for child in root.childs:
+            visited.append(child.state)
             v.append(min(v_min, max_value(child)))
         
         result = min(v)
-        print("main: min_value " + str(v) + " is selected")
-
-        return result
+        ind = v.index(result)
+        return result, root.childs[ind].action, visited
 
 def max_value(currentNode):
     # leaf node
     if len(currentNode.childs) == 0:
-        print("max_value")
-        print(currentNode.state, currentNode.value)
         return currentNode.value
     
     v = -2000000
 
     for child in currentNode.childs:
+        visited.append(child.state)
         v = max(v, min_value(child))
-        print("max_value " + str(v) + " is selected")
 
     return v
 
 def min_value(currentNode):
     if len(currentNode.childs) == 0:
-        print("min_value")
-        print(currentNode.state, currentNode.value)
         return currentNode.value
 
     v = 2000000
 
     for child in currentNode.childs:
+        visited.append(child.state)
         v = min(v, max_value(child))
-        print("min_value " + str(v) + " is selected")
     
     return v
 
@@ -286,5 +360,6 @@ def SolveGame(methodName, problemFileName, playerType):
     # f = minimax_decision(playerType)
     return
 
-print(SolveGame("s", "tictactoe1.txt", "MAX"))
-print(expand(root.state, 'X'))
+# print(SolveGame("s", "tictactoe2.txt", "MAX"))
+SolveGame('s', "gametree1.txt", 'MAX')
+print(minimax_decision("MIN"))
