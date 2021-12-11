9e6d7e7a20b16a68aebf9eef35e973a1f0ec4fa0
erndmrcn <erndmrcn42@gmail.com>
2021-12-11 20:28:13 +0300

    HW3 is done
    HW3 is done

--- hw3.py
+++ hw3.py
@@ -5,8 +5,6 @@
 # global variables and structures
 class Node:
     def __init__(self, parent, min_max, state, action, value):
-        # parent -> parent of the node
-        # min_max -> if the node is min or max 
         self.parent = parent
         self.min_max = min_max
         self.state = state
@@ -15,11 +13,11 @@ class Node:
         self.childs = []
         return
 
-global stateNodes
-# holds (state, Node)
 stateNodes = []
 visited = []
 
+# utility functions
+# deep copy for child expansion
 def deepCopy(arr):
     result = []
     for row in arr:
@@ -29,84 +27,105 @@ def deepCopy(arr):
         result.append(temp)
     return result
 
+# changes state array to state string
+def arrayToState(arr):
+    result = ""
+    for e in arr:
+        for a in e:
+            result += a
+
+    return result
+
+# changes state string to state array
+def stateToArray(state):
+    result = []
+    for i in range(3):
+        temp = []
+        for j in range(3):
+            temp.append(state[i*3 + j])
+        result.append(temp)
+    return result
+
+# checks if a state is end state or not
 def terminalState(state):
     visited.append(state)
 
     row1 = state[:3]
+    # three 'X' or 'O' in row1
     if row1[0] == row1[1] and row1[1] == row1[2]:
-        # terminate
         if row1[0] == 'X':
             return 'X'
-        else:
+        elif row1[0] == 'O':
             return 'Y'
 
     row2 = state[3:6]
+    # three 'X' or 'O' in row2
     if row2[0] == row2[1] and row2[1] == row2[2]:
-        # terminate
         if row2[0] == 'X':
             return 'X'
-        else:
+        elif row2[0] == 'O':
             return 'Y'
 
     row3 = state[6:9]
+    # three 'X' or 'O' in row2
     if row3[0] == row3[1] and row3[1] == row3[2]:
-        # terminate
         if row3[0] == 'X':
             return 'X'
-        else:
+        elif row3[0] == 'O':
             return 'Y'
 
     diagonal1 = state[0::4]
+    # three 'X' or 'O' in diagonal1
     if diagonal1[0] == diagonal1[1] and diagonal1[1] == diagonal1[2]:
-        # terminage
         if diagonal1[0] == 'X':
             return 'X'
-        else:
+        elif diagonal1[0] == 'O':
             return 'Y'
 
     diagonal2 = state[2:8:2]
+    # three 'X' or 'O' in diagonal2
     if diagonal2[0] == diagonal2[1] and diagonal2[1] == diagonal2[2]:
         # terminage
         if diagonal2[0] == 'X':
             return 'X'
-        else:
+        elif diagonal2[0] == 'O':
             return 'Y'  
 
     column1 = state[0::3]
+    # three 'X' or 'O' in column1
     if column1[0] == column1[1] and column1[1] == column1[2]:
-        # terminate
         if column1[0] == 'X':
             return 'X'
-        else:
+        elif column1[0] == 'O':
             return 'Y'
 
     column2 = state[1::3]
+    # three 'X' or 'O' in column2
     if column2[0] == column2[1] and column2[1] == column2[2]:
-        # terminate
         if column2[0] == 'X':
             return 'X'
-        else:
+        elif column2[0] == 'O':
             return 'Y'
 
     column3 = state[2::3]
+    # three 'X' or 'O' in column3
     if column3[0] == column3[1] and column3[1] == column3[2]:
-        # terminate
         if column3[0] == 'X':
             return 'X'
-        else:
+        elif column3[0] == 'O':
             return 'Y'
 
+    # there is no winner
+    # board is full
     if ' ' not in state:
         return 'BF'
     
     # non terminal
     return 'NT'
 
-
+# expand a tictactoe node
 def expand(currentNode, depth):
-    # Order -> (0, 0), (1, 0), (2, 0)
-    #          (0, 1), (1, 1), (2, 1)
-    #          (0, 2), (1, 2), (2, 2)
+    # determine whose turn it is
     if depth % 2 == 0:
         symbol = 'X'
     elif depth % 2 == 1:
@@ -115,6 +134,7 @@ def expand(currentNode, depth):
     childs = []
     arr = stateToArray(currentNode.state)
     
+    # first row
     if arr[0][0] == ' ':
         tempArr = deepCopy(arr)
         tempArr[0][0] = symbol
@@ -131,7 +151,7 @@ def expand(currentNode, depth):
         newNode = Node(currentNode, None, arrayToState(tempArr), (2,0), None)
         childs.append(newNode)
 
-
+    # second row
     if arr[1][0] == ' ':
         tempArr = deepCopy(arr)
         tempArr[1][0] = symbol
@@ -148,7 +168,7 @@ def expand(currentNode, depth):
         newNode = Node(currentNode, None, arrayToState(tempArr), (2,1), None)
         childs.append(newNode)
 
-
+    # third row
     if arr[2][0] == ' ':
         tempArr = deepCopy(arr)
         tempArr[2][0] = symbol
@@ -167,26 +187,7 @@ def expand(currentNode, depth):
 
     return childs
 
-# utilities
-def arrayToState(arr):
-    result = ""
-    for e in arr:
-        for a in e:
-            result += a
-
-    return result
-
-def stateToArray(state):
-    result = []
-    for i in range(3):
-        temp = []
-        for j in range(3):
-            temp.append(state[i*3 + j])
-        result.append(temp)
-    return result
-
-# main function
-
+# read files into the global structures
 def readFile(fileName):
     global root, stateNodes
 
@@ -244,25 +245,37 @@ def readFile(fileName):
         exit('Program exited with code -1')
     return
 
-
-def minimax_decision_ttt():
-    global visited
+# minimax for tictactoe
+def minimax_decision_ttt(alpha_beta):
     v_max = -2000000
+    v_min = 2000000
     v = []
+    
     depth = 0
+
     childs = expand(root, depth)
     for child in childs:
         root.childs.append(child)
 
     for child in root.childs:
-        v.append(max(v_max, min_value_ttt(child, depth + 1)))
+        # apply alphabeta pruning
+        if alpha_beta == True:
+            temp = max(v_max, min_value_ttt(child, depth+1, alpha_beta, v_max, v_min))
+            if temp > v_max:
+                v_max = temp
+            if v_min <= v_max:
+                break
+            v.append(v_max)
+        else:
+            v.append(max(v_max, min_value_ttt(child, depth + 1, alpha_beta, v_max, v_min)))
 
     result = max(v)
+    # find the action taken for optimal solution
     ind = v.index(result)
     return result, root.childs[ind].action, visited
-    
 
-def max_value_ttt(currentNode, depth):
+# max value for tictactoe
+def max_value_ttt(currentNode, depth, alpha_beta, alpha, beta):
     result = terminalState(currentNode.state)
     if result == 'X':
         # X wins
@@ -280,11 +293,21 @@ def max_value_ttt(currentNode, depth):
         currentNode.childs.append(child)
 
     for child in currentNode.childs:
-        v = max(v, min_value_ttt(child, depth+1))
+        # apply alphabeta pruning
+        if alpha_beta == True:
+            v = max(v, min_value_ttt(child, depth+1, alpha_beta, alpha, beta))
+            if v > alpha:
+                alpha = v
+            if beta <= alpha:
+                break
+        else:
+            v = max(v, min_value_ttt(child, depth+1, alpha_beta, alpha, beta))
+            alpha = v
 
-    return v
+    return alpha
 
-def min_value_ttt(currentNode, depth):
+# min value for tictactoe
+def min_value_ttt(currentNode, depth, alpha_beta, alpha, beta):
     result = terminalState(currentNode.state)
     if result == 'X':
         # X wins
@@ -302,32 +325,71 @@ def min_value_ttt(currentNode, depth):
         currentNode.childs.append(child)
 
     for child in currentNode.childs:
-        v = min(v, max_value_ttt(child, depth+1))
+        # apply alphabeta pruning
+        if alpha_beta == True:
+            v = min(v, max_value_ttt(child, depth+1, alpha_beta, alpha, beta))
+            if v < beta:
+                beta = v
+            if beta <= alpha:
+                break
+        else:
+            v = min(v, max_value_ttt(child, depth+1, alpha_beta, alpha, beta))
+            beta = v
+    return beta
 
-    return v
 
-def minimax_decision(min_max):
+# minimax for gametree
+def minimax_decision(min_max, alpha_beta):
+    v_max = 999999
+    v_min = -999999
+    v = []
     v_max = -2000000
     v_min = 2000000
     v = []
+    depth = 0
+
+
     if min_max == "MAX":
         for child in root.childs:
             visited.append(child.state)
-            v.append(max(v_max, min_value(child)))
+            # apply alphabeta pruning
+            if alpha_beta == True:
+                temp = max(v_max, min_value(child, alpha_beta, v_max, v_min))
+                if temp > v_max:
+                    v_max = temp
+                if v_min <= v_max:
+                    break
+                v.append(v_max)
+            else:
+                v.append(max(v_max, min_value(child, alpha_beta, v_max, v_min)))
 
-        result = max(v)    
+        result = max(v)
+        # find the action taken for optimal solution
         ind = v.index(result)
         return result, root.childs[ind].action, visited
+
     elif min_max == "MIN":
         for child in root.childs:
             visited.append(child.state)
-            v.append(min(v_min, max_value(child)))
-        
+            # apply alphabeta pruning
+            if alpha_beta == True:
+                temp = min(v_min, max_value(child, alpha_beta, v_max, v_min))
+                if temp < v_min:
+                    v_min = temp
+                if v_min <= v_max:
+                    break
+                v.append(v_min)
+            else:
+                v.append(min(v_min, max_value(child, alpha_beta, v_max, v_min)))
+
         result = min(v)
+        # find the action taken for optimal solution
         ind = v.index(result)
         return result, root.childs[ind].action, visited
 
-def max_value(currentNode):
+
+# max value for gametree
+def max_value(currentNode, alpha_beta, alpha, beta):
     # leaf node
     if len(currentNode.childs) == 0:
         return currentNode.value
@@ -336,11 +398,20 @@ def max_value(currentNode):
 
     for child in currentNode.childs:
         visited.append(child.state)
-        v = max(v, min_value(child))
+        v = max(v, min_value(child, alpha_beta, alpha, beta))
+        # apply alphabeta pruning
+        if alpha_beta == True:
+            if v > alpha:
+                alpha = v
+            if beta <= alpha:
+                break
+        else:
+            alpha = v
+    return alpha
 
-    return v
+# min value for gametree
+def min_value(currentNode, alpha_beta, alpha, beta):
 
-def min_value(currentNode):
     if len(currentNode.childs) == 0:
         return currentNode.value
 
@@ -348,18 +419,38 @@ def min_value(currentNode):
 
     for child in currentNode.childs:
         visited.append(child.state)
-        v = min(v, max_value(child))
-    
-    return v
+        v = min(v, max_value(child, alpha_beta, alpha, beta))
+        # apply alphabeta pruning
+        if alpha_beta == True:
+            if v < beta:
+                beta = v
+            if beta <= alpha:
+                break
+        else:
+            beta = v
+    return beta
 
+# main function to be calles
 def SolveGame(methodName, problemFileName, playerType):
-    # methodName - Minimax or AlphaBeta
-    # fileName - gametree or tictactoe
-    # playerType - MAX or MIN
-    readFile(problemFileName)
-    # f = minimax_decision(playerType)
-    return
+    global visited, stateNodes 
 
-# print(SolveGame("s", "tictactoe2.txt", "MAX"))
-SolveGame('s', "gametree1.txt", 'MAX')
-print(minimax_decision("MIN"))
+    visited = []
+    stateNodes = []
+
+    readFile(problemFileName)
+
+    # check if alphabeta pruning will be applied
+    if methodName == "Minimax":
+        ab = False
+    elif methodName == "AlphaBeta":
+        ab = True
+
+    # call problem specifi functions according to fileName
+    if problemFileName[0] == 'g':
+        result = minimax_decision(playerType, ab)
+    elif problemFileName[0] == 't':
+        result = minimax_decision_ttt(ab)
+    else:
+        exit("Wrong file name format")
+
+    return result
